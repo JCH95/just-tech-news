dddee1c8baa1e0acf4ae331a03ddacdd04afb9cd
JCH95 <jhorger95@gmail.com>
2022-04-05 10:41:11 -0500

    added login and logout stuff

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,7 +27,6 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbPostData => {
-            console.log(dbPostData[0]);
             const posts = dbPostData.map(post => post.get({ plain: true }));
             // pass a single post object into the homepage template
             // res.render('homepage', dbPostData[0].get({ plain: true }));
@@ -37,6 +36,16 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    console.log(req.session);
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
